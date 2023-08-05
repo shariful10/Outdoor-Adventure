c1b59faf4c6f400177878695f211e97b16ab5a03
rihan010 <ibrhimrihan000@gmail.comm>
2023-08-05 23:39:21 +0600

    Fixed Section Title

--- src/Components/Shared/SectionHeader/SectionHeader.jsx
+++ src/Components/Shared/SectionHeader/SectionHeader.jsx
@@ -3,19 +3,16 @@ import React from "react";
 const SectionHeader = ({ bgImage, subTitle, title }) => {
   return (
     <section
-      className="relative h-[400px] bg-cover bg-center text-white bg-fixed"
+      className="relative h-[50vh] md:h-[90vh] bg-cover bg-center text-white bg-fixed"
       style={{
         backgroundImage: `url(${bgImage})`,
       }}
     >
-      <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
+      <div className="absolute inset-0 flex flex-col text-center items-center justify-center z-10">
         <h1 className="font-bold mb-4 font-Montserrat text-2xl my-5">
           {subTitle}
         </h1>
-        <p className="text-7xl font-bold font-Montserrat">{title}</p>
-        <button className="bg-primary px-10 py-3 rounded-full font-semibold font-NotoSans my-10">
-          Learn More
-        </button>
+        <p className="text-5xl md:text-7xl font-bold font-Montserrat">{title}</p>
       </div>
       <div className="absolute inset-0 bg-black opacity-50"></div>
     </section>
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import WaterSports from './WaterSports/WaterSports';
 import WinterSports from './WinterSports/WinterSports';
 import Camping from './Camping/Camping';
+import SectionHeader from '../../Components/Shared/SectionHeader/SectionHeader'
 
 const Projects = () => {
     return (
         <div>
+            <SectionHeader bgImage="/project-background.png" title={"Our Projects"}></SectionHeader>
             <WaterSports></WaterSports>
             <WinterSports></WinterSports>
             <Camping></Camping>
