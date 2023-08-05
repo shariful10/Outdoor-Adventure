b1f226e4314d2e88653835bbba8905eeede1c163
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-08-05 21:48:51 +0600

    Container Remodify

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Container from "./../Components/Shared/Container/Container";
 import Navbar from "./../Components/Shared/Navbar/Navbar";
 import Footer from "../Components/Shared/Footer/Footer";
 import { Outlet } from "react-router-dom";
@@ -7,11 +6,9 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
 	return (
 		<div className="">
-			<Container>
 				<Navbar />
 				<Outlet />
 				<Footer />
-			</Container>
 		</div>
 	);
 };
