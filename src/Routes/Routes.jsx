15f9487043a389e531d64e670255ccb888345ae6
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-08-06 00:03:34 +0600

    Signup Page Added With Firebase Authentication

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,24 +3,29 @@ import Main from "../Layouts/Main";
 import AllSection from "../Pages/Home/AllSection/AllSection";
 import Projects from "../Pages/Projects/Projects";
 import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Main />,
-        children: [
-            {
-                path: "/",
-                element: <AllSection />,
-            },
-            {
-                path: "/login",
-                element: <Login />,
-            },
-            {
-                path: "/projects",
-                element: <Projects />,
-            },
-        ],
-    },
+	{
+		path: "/",
+		element: <Main />,
+		children: [
+			{
+				path: "/",
+				element: <AllSection />,
+			},
+			{
+				path: "/projects",
+				element: <Projects />,
+			},
+		],
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/signup",
+		element: <SignUp />,
+	},
 ]);
