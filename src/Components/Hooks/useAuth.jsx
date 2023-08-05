cfa45ea42250d88ac3525f678b2e35802c741da6
Wixden <watchonlytutorial@gmail.com>
2023-08-05 22:47:17 +0600

    Banner Section Complete

--- src/Components/Hooks/useAuth.jsx
+++ src/Components/Hooks/useAuth.jsx
@@ -2,8 +2,8 @@ import { useContext } from "react";
 import AuthProvider from "../../Providers/AuthProvider";
 
 const useAuth = () => {
-    const auth = useContext(AuthProvider);
-    return auth
+  const auth = useContext(AuthProvider);
+  return auth;
 };
 
 export default useAuth;
--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import Container from "./../Components/Shared/Container/Container";
 import Navbar from "./../Components/Shared/Navbar/Navbar";
 import Footer from "../Components/Shared/Footer/Footer";
 import { Outlet } from "react-router-dom";
 
 const Main = () => {
-	return (
-		<div className="">
-			<Container>
-				<Navbar />
-				<Outlet />
-				<Footer />
-			</Container>
-		</div>
-	);
+  return (
+    <div className="">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
