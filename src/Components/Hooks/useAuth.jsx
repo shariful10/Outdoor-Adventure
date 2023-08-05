ada52a908e21498aa882503e50787dde3eca3324
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-08-05 23:05:51 +0600

    Login Page Added With Firebase Authentication

--- src/Components/Hooks/useAuth.jsx
+++ src/Components/Hooks/useAuth.jsx
@@ -3,8 +3,8 @@ import { useContext } from "react";
 import AuthProvider from "../../Providers/AuthProvider";
 
 const useAuth = () => {
-    const auth = useContext(AuthProvider);
-    return auth
+  const auth = useContext(AuthProvider);
+  return auth;
 };
 
 export default useAuth;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import AllSection from "../Pages/Home/AllSection/AllSection";
+<<<<<<< HEAD
 import Login from "../Pages/Login/Login";
+=======
+import Projects from "../Pages/Projects/Projects";
+>>>>>>> 873962353d544e8da6e49c6108ba86c490757d2f
 
 export const router = createBrowserRouter([
 	{
@@ -13,8 +17,13 @@ export const router = createBrowserRouter([
 				element: <AllSection />,
 			},
 			{
+<<<<<<< HEAD
 				path: "/login",
 				element: <Login />,
+=======
+				path: "/projects",
+				element: <Projects />,
+>>>>>>> 873962353d544e8da6e49c6108ba86c490757d2f
 			},
 		],
 	},
