0111d8ec1a3cacf7923df2f276308cc49ce2fb46
taslima97 <antora.taslima97@gmail.com>
2023-08-06 12:40:50 +0600

    hello

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import image from '../../assets/image/outdoor-image-02.jpg'
 import BottomAbout from './BottomAbout';
+import SectionHeader from '../../Components/Shared/SectionHeader/SectionHeader';
+import bg from '../../assets/image/about-bg-5.jpg'
+import Container from '../../Components/Shared/Container/Container';
 
 const About = () => {
     return (
-        <div className='mx-16'>
-        <div className='flex justify-between'>
-            <div>
-                <h3 className='text-3xl font-bold'>OUR MISSION</h3>
-                <div className='text-5xl'>
-<hr />
-                </div>
-                <p className='my-8'>Click edit button to change this text. Lorem ipsum dolor sit amet, <br /> adipiscing elit.</p>
-                <p>Click edit button to change this text. Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, <br /> pulvinar dapibus leo.</p>
+        <>
+            <SectionHeader
+                title={'WHO WE ARE?'} bgImage={bg}
+            ></SectionHeader>
+            <div className='mx-16'>
+
+                <Container>
+                    <div className='md:flex justify-between my-24'>
+                        <div>
+                            <h3 className='text-3xl font-bold'>OUR MISSION</h3>
+                            
+                            <p className="bg-rose-600 w-20 h-1 my-3">&nbsp;</p>
+                            
+                            <p className='my-8'>Click edit button to change this text. Lorem ipsum dolor sit amet, <br /> adipiscing elit.</p>
+                            <p>Click edit button to change this text. Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, <br /> pulvinar dapibus leo.</p>
+                        </div>
+                        <div>
+                            <img className='custom-shadow' style={{ height: 400 }} src={image} alt="" />
+                        </div>
+
+                    </div>
+                    <BottomAbout></BottomAbout>
+                </Container>
             </div>
-            <div>
-                <img style={{height:400}} src={image} alt="" />
-            </div>
-            
-        </div>
-        <BottomAbout></BottomAbout>
-        </div>
+
+        </>
 
     );
 };
--- src/Pages/About/BottomAbout.jsx
+++ src/Pages/About/BottomAbout.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const BottomAbout = () => {
     return (
-        <div className='flex justify-between my-24'>
-            <div style={{width: "50%"}}>
+        <div className='md:flex justify-between my-24'>
+            <div className='md:w-1/2 w-full'>
                 <h3 className='text-2xl font-semibold'>Extraordinary Experiences</h3>
-                <hr />
+                <p className="bg-rose-600 w-20 h-1 my-3">&nbsp;</p>
                 <p className='my-8'>Click edit button to change this text. Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.</p>
                 <p>Click edit button to change this text. Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, <br /> pulvinar dapibus leo.</p>
             </div>
-            <div style={{width: "50%"}}>
+            <div className='md:w-1/2 w-full'>
                 <h3 className='text-2xl font-semibold'>Our Core Values</h3>
                 <div>
-                    <hr />
+                    <p className="bg-rose-600 w-20 h-1 my-3">&nbsp;</p>
                 </div>
                 <p className='my-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                 <ul>
